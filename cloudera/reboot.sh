83cce9faa3d7606930949f4b47bca640213a8923
杜森 <sen.du@petgleam.com>
2023-04-16 18:51:16 +0800

    [ADD] Reboot

---
+++ cloudera/reboot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#author: Sen Du
+#email: dusen@gennlife.com
+#created: 2023-04-16 21:00:00
+#updated: 2023-04-16 21:00:00
+
+set -e 
+
+# 重启机器
+function reboot() {
+    echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do echo $ipaddr; ssh -n $ipaddr "reboot"; done$CEND"
+    cat config/vm_info | while read ipaddr name passwd; do echo $ipaddr; ssh -n $ipaddr "reboot"; done
+}
+
+function main() {
+    reboot
+}
+
+main
