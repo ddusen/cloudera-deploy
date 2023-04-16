4b644962d37f1b977131b6a6b6b257700d6c12d3
杜森 <sen.du@petgleam.com>
2023-04-16 19:13:07 +0800

    [FIX] # 配置 ntp server，避免配置覆盖

--- cloudera/06_ntp.sh
+++ cloudera/06_ntp.sh
@@ -25,16 +25,16 @@ function backup_ntp_config() {
     cat config/vm_info | while read ipaddr name passwd; do echo $ipaddr; ssh -n $ipaddr "cp /etc/ntp.conf /etc/ntp.conf.bak"; done
 }
 
-# 配置 ntp server
-function config_ntp_server() {
-    echo -e "$CSTART>>>>cp config/ntp_server /etc/ntp.conf$CEND"
-    cp config/ntp_server /etc/ntp.conf
-}
-
 # 配置 ntp clients
 function config_ntp_clients() {
     echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do scp config/ntp_clients \$ipaddr:/etc/ntp.conf; done$CEND"
     cat config/vm_info | while read ipaddr name passwd; do scp config/ntp_clients $ipaddr:/etc/ntp.conf; done
+}
+
+# 配置 ntp server
+function config_ntp_server() {
+    echo -e "$CSTART>>>>cp config/ntp_server /etc/ntp.conf$CEND"
+    cp config/ntp_server /etc/ntp.conf
 }
 
 # 重启 ntp 服务
