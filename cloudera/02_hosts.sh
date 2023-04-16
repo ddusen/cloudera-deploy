b10f0d65c60c202730b0301bddb0a4576190f6b2
杜森 <sen.du@petgleam.com>
2023-04-16 16:00:11 +0800

    [FIX] cat: all_nodes: No such file or directory

--- cloudera/02_hosts.sh
+++ cloudera/02_hosts.sh
@@ -9,12 +9,13 @@ set -e
 
 # 配置所有节点的 hosts
 function config_hosts() {
-    echo -e '\t\t for i in `cat all_nodes`; do scp config/hosts $i:/etc/ done'
-    for i in `cat all_nodes`; do scp config/hosts $i:/etc/ ; done
+    echo -e '\t\t for i in `cat config/all_nodes`; do scp config/hosts $i:/etc/ done'
+    for i in `cat config/all_nodes`; do scp config/hosts $i:/etc/ ; done
 }
 
 function main() {
     echo '02_hosts.sh'
+    
     echo -e '\t config_hosts'
     config_hosts
 }
