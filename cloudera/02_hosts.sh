c50d811c8364a7205640027409a32cb1600a6ca2
杜森 <sen.du@petgleam.com>
2023-04-16 12:10:16 +0800

    [ADD] 2. 配置所有节点的 hosts

---
+++ cloudera/02_hosts.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#author: Sen Du
+#email: dusen@gennlife.com
+#created: 2023-04-16 11:00:00
+#updated: 2023-04-16 11:00:00
+
+set -e 
+
+# 配置所有节点的 hosts
+function config_hosts() {
+    echo -e '\t\t for i in `cat all_nodes`; do scp config/hosts $i:/etc/ done'
+    for i in `cat all_nodes`; do scp config/hosts $i:/etc/ done
+}
+
+function main() {
+    echo '02_hosts.sh'
+    echo -e '\t config_hosts'
+    config_hosts
+}
+
+main
