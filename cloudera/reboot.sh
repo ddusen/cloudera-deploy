d98636e95f55964098d664ddf4ec5035cf7d5189
杜森 <sen.du@petgleam.com>
2023-04-16 18:53:21 +0800

    [IMP] reboot || true

--- cloudera/reboot.sh
+++ cloudera/reboot.sh
@@ -10,7 +10,7 @@ set -e
 # 重启机器
 function reboot() {
     echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do echo $ipaddr; ssh -n $ipaddr "reboot"; done$CEND"
-    cat config/vm_info | while read ipaddr name passwd; do echo $ipaddr; ssh -n $ipaddr "reboot || true"; done
+    cat config/vm_info | while read ipaddr name passwd; do echo $ipaddr; ssh -n $ipaddr "reboot" || true; done
 }
 
 function main() {
