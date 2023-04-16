11840c02178f621fd31c99ef2e0db00059043eaa
杜森 <sen.du@petgleam.com>
2023-04-16 12:10:41 +0800

    [ADD] 4. 安装 httpd

---
+++ cloudera/04_httpd.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#author: Sen Du
+#email: dusen@gennlife.com
+#created: 2023-04-16 10:00:00
+#updated: 2023-04-16 10:00:00
+
+set -e 
+
+FILENAME="cloudera.6.3.tar.gz"
+
+# 下载 cm、cdh 相关的软件
+function download_files() {
+    # wget
+}
+
+# 解压到指定目录
+function unzip_files() {
+    dist_dir="/var/www/html/cloudera"
+    echo -e "\t\t mkdir -p $dist_dir && tar -zxvf /opt/$FILENAME -C $dist_dir"
+    mkdir -p $dist_dir && tar -zxvf /opt/$FILENAME -C $dist_dir
+}
+
+# 安装 httpd：用作私有化 cm、cdh 的软件仓库
+function install_httpd() {
+    echo -e "\t\t yum install -y httpd"
+    yum install -y httpd
+}
+
+# 启动 httpd
+function start_httpd() {
+    echo -e "\t\t systemctl stop firewalld; systemctl enable httpd; systemctl start httpd"
+    systemctl stop firewalld
+    systemctl enable httpd
+    systemctl start httpd
+}
+
+function main() {
+    echo "04_httpd.sh"
+    echo -e "\t download_files"
+    download_files
+
+    echo -e "\t unzip_files"
+    unzip_files
+
+    echo -e "\t install_httpd"
+    install_httpd
+
+    echo -e "\t start_httpd"
+    start_httpd
+}
