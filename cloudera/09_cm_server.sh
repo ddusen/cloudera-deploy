915232782aa6d5a6bc801b2eed360fe9b786da58
杜森 <sen.du@petgleam.com>
2023-04-16 19:43:35 +0800

    [ADD] 添加日志

--- cloudera/09_cm_server.sh
+++ cloudera/09_cm_server.sh
@@ -9,18 +9,25 @@ set -e
 
 # 安装 cloudera manager server
 function install_server() {
+    echo -e "$CSTART>>>>rpm -ivh http://$HTTPD_SERVER/cloudera/cm6/6.3.1/cloudera-manager-server-6.3.1-1466458.el7.x86_64.rpm || true$CEND"
     rpm -ivh http://$HTTPD_SERVER/cloudera/cm6/6.3.1/cloudera-manager-server-6.3.1-1466458.el7.x86_64.rpm || true
 }
 
 # 配置 cloudera manager server
 function config_server() {
+    echo -e "$CSTART>>>>cp /etc/cloudera-scm-server/db.properties /etc/cloudera-scm-server/db.properties.bak$CEND"
     cp /etc/cloudera-scm-server/db.properties /etc/cloudera-scm-server/db.properties.bak
+
+    echo -e "$CSTART>>>>cp config/cm_server /etc/cloudera-scm-server/db.properties$CEND"
     cp config/cm_server /etc/cloudera-scm-server/db.properties
+
+    echo -e "$CSTART>>>>chmod 644 /etc/cloudera-scm-server/db.properties$CEND"
     chmod 644 /etc/cloudera-scm-server/db.properties
 }
 
 # 重启 cloudera manager server
 function restart_server() {
+    echo -e "$CSTART>>>>systemctl restart cloudera-scm-server; systemctl enable cloudera-scm-server$CEND"
     systemctl restart cloudera-scm-server; systemctl enable cloudera-scm-server
 }
 
@@ -29,13 +36,13 @@ function main() {
 	
     echo -e "$CSTART>09_cm_server.sh$CEND"
 
-    echo -e "$CSTART>>>>install_server$CEND"
+    echo -e "$CSTART>>install_server$CEND"
     install_server
 
-    echo -e "$CSTART>>>>config_server$CEND"
+    echo -e "$CSTART>>config_server$CEND"
     config_server
 
-    echo -e "$CSTART>>>>restart_server$CEND"
+    echo -e "$CSTART>>restart_server$CEND"
     restart_server
 }
 
