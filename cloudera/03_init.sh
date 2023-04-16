b8bbfe2ed6e7af5685ea6c126b8baed8c4ce9ff9
杜森 <sen.du@petgleam.com>
2023-04-16 17:10:52 +0800

    [FIX] 字符转义

--- cloudera/03_init.sh
+++ cloudera/03_init.sh
@@ -9,86 +9,86 @@ set -e
 
 # 安装一些基础软件，便于后续操作
 function install_base() {
-    echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr "yum install -y wget net-tools epel-release htop"; done$CEND"
+    echo -e `$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr "yum install -y wget net-tools epel-release htop"; done$CEND`
     cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr "yum install -y wget net-tools epel-release htop"; done
 }
 
 # 备份一些配置文件
 function backup_configs(){
-    echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr `mkdir -p /opt/backup_sys_configs;`; done$CEND"
-    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr `mkdir -p /opt/backup_sys_configs;`; done
+    echo -e `$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr "mkdir -p /opt/backup_sys_configs;"; done$CEND`
+    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr "mkdir -p /opt/backup_sys_configs;"; done
 
-    echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr `cp /etc/security/limits.conf /opt/backup_sys_configs;`; done$CEND"
-    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr `cp /etc/security/limits.conf /opt/backup_sys_configs;`; done
+    echo -e `$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr "cp /etc/security/limits.conf /opt/backup_sys_configs;"; done$CEND`
+    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr "cp /etc/security/limits.conf /opt/backup_sys_configs;"; done
 
-    echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr `cp /etc/security/limits.d/20-nproc.conf /opt/backup_sys_configs;`; done$CEND"
-    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr `cp /etc/security/limits.d/20-nproc.conf /opt/backup_sys_configs;`; done
+    echo -e `$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr "cp /etc/security/limits.d/20-nproc.conf /opt/backup_sys_configs;"; done$CEND`
+    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr "cp /etc/security/limits.d/20-nproc.conf /opt/backup_sys_configs;"; done
 
-    echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr `cp /etc/sysctl.conf /opt/backup_sys_configs;`; done$CEND"
-    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr `cp /etc/sysctl.conf /opt/backup_sys_configs;`; done
+    echo -e `$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr "cp /etc/sysctl.conf /opt/backup_sys_configs;"; done$CEND`
+    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr "cp /etc/sysctl.conf /opt/backup_sys_configs;"; done
 
-    echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr `cp /etc/ssh/sshd_config /opt/backup_sys_configs`; done$CEND"
-    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr `cp /etc/ssh/sshd_config /opt/backup_sys_configs`; done
+    echo -e `$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr "cp /etc/ssh/sshd_config /opt/backup_sys_configs"; done$CEND`
+    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr "cp /etc/ssh/sshd_config /opt/backup_sys_configs"; done
 }
 
 # 禁用 hugepage
 function disable_hugepage(){
-    echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr `grubby --update-kernel=ALL --args="transparent_hugepage=never"`; done$CEND"
-    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr `grubby --update-kernel=ALL --args="transparent_hugepage=never"`; done
+    echo -e `$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr "grubby --update-kernel=ALL --args='transparent_hugepage=never'"; done$CEND`
+    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr "grubby --update-kernel=ALL --args='transparent_hugepage=never'"; done
 
-    echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr `sed -i "/^#RemoveIPC=no/cRemoveIPC=no" /etc/systemd/logind.conf; systemctl restart systemd-logind.service`; done$CEND"
-	cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr `sed -i "/^#RemoveIPC=no/cRemoveIPC=no" /etc/systemd/logind.conf; systemctl restart systemd-logind.service`; done
+    echo -e `$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr "sed -i '/^#RemoveIPC=no/cRemoveIPC=no' /etc/systemd/logind.conf; systemctl restart systemd-logind.service"; done$CEND`
+	cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr "sed -i '/^#RemoveIPC=no/cRemoveIPC=no' /etc/systemd/logind.conf; systemctl restart systemd-logind.service"; done
 }
 
 # 关闭 selinux
 function disable_selinux(){
-	echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr `sed -i "/^SELINUX=/cSELINUX=disabled" /etc/selinux/config;`; done$CEND"
-    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr `sed -i "/^SELINUX=/cSELINUX=disabled" /etc/selinux/config;`; done
+	echo -e `$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr "sed -i '/^SELINUX=/cSELINUX=disabled" /etc/selinux/config;"; done$CEND`
+    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr "sed -i '/^SELINUX=/cSELINUX=disabled" /etc/selinux/config;"; done
 }
 
 # 配置ssh
 function config_ssh(){
-	echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr `sed -i "/^#UseDNS/cUseDNS no" /etc/ssh/sshd_config;`; done$CEND"
-    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr `sed -i "/^#UseDNS/cUseDNS no" /etc/ssh/sshd_config;`; done
+	echo -e `$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr "sed -i '/^#UseDNS/cUseDNS no' /etc/ssh/sshd_config;"; done$CEND`
+    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr "sed -i '/^#UseDNS/cUseDNS no' /etc/ssh/sshd_config;"; done
 
-	echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr `sed -i "/^GSSAPIAuthentication/cGSSAPIAuthentication no" /etc/ssh/sshd_config;`; done$CEND"
-    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr `sed -i "/^GSSAPIAuthentication/cGSSAPIAuthentication no" /etc/ssh/sshd_config;`; done
+	echo -e `$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr "sed -i '/^GSSAPIAuthentication/cGSSAPIAuthentication no' /etc/ssh/sshd_config;"; done$CEND`
+    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr "sed -i '/^GSSAPIAuthentication/cGSSAPIAuthentication no' /etc/ssh/sshd_config;"; done
 
-	echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr `sed -i "/^GSSAPICleanupCredentials/cGSSAPICleanupCredentials no" /etc/ssh/sshd_config;`; done$CEND"
-    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr `sed -i "/^GSSAPICleanupCredentials/cGSSAPICleanupCredentials no" /etc/ssh/sshd_config;`; done
+	echo -e `$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr "sed -i '/^GSSAPICleanupCredentials/cGSSAPICleanupCredentials no' /etc/ssh/sshd_config;"; done$CEND`
+    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr "sed -i '/^GSSAPICleanupCredentials/cGSSAPICleanupCredentials no' /etc/ssh/sshd_config;"; done
 
-	echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr `sed -i "/^#MaxStartups/cMaxStartups 10000:30:20000" /etc/ssh/sshd_config;`; done$CEND"
-    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr `sed -i "/^#MaxStartups/cMaxStartups 10000:30:20000" /etc/ssh/sshd_config;`; done
+	echo -e `$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr "sed -i '/^#MaxStartups/cMaxStartups 10000:30:20000" /etc/ssh/sshd_config;"; done$CEND`
+    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr "sed -i '/^#MaxStartups/cMaxStartups 10000:30:20000" /etc/ssh/sshd_config;"; done
 }
 
 # 配置网络策略
 function config_network(){
-	echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr `chkconfig iptables off; chkconfig ip6tables off; chkconfig postfix off;`; done$CEND"
-    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr `chkconfig iptables off; chkconfig ip6tables off; chkconfig postfix off;`; done
+	echo -e `$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr `chkconfig iptables off; chkconfig ip6tables off; chkconfig postfix off;"; done$CEND`
+    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr `chkconfig iptables off; chkconfig ip6tables off; chkconfig postfix off;"; done
 
-	echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr `systemctl disable postfix; systemctl disable libvirtd; systemctl disable firewalld;`; done$CEND"
-    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr `systemctl disable postfix; systemctl disable libvirtd; systemctl disable firewalld;`; done
+	echo -e `$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr `systemctl disable postfix; systemctl disable libvirtd; systemctl disable firewalld;"; done$CEND`
+    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr `systemctl disable postfix; systemctl disable libvirtd; systemctl disable firewalld;"; done
 }
 
 function main() {
-    echo -e "$CSTAET>03_init.sh$CEND"
+    echo -e `$CSTAET>03_init.sh$CEND`
 
-    echo -e "$CSTAET>>install_base$CEND"
+    echo -e `$CSTAET>>install_base$CEND`
     install_base
 
-    echo -e "$CSTAET>>backup_configs$CEND"
+    echo -e `$CSTAET>>backup_configs$CEND`
     backup_configs
 
-    echo -e "$CSTAET>>disable_hugepage$CEND"
+    echo -e `$CSTAET>>disable_hugepage$CEND`
     disable_hugepage
 
-    echo -e "$CSTAET>>disable_selinux$CEND"
+    echo -e `$CSTAET>>disable_selinux$CEND`
     disable_selinux
 
-    echo -e "$CSTAET>>config_ssh$CEND"
+    echo -e `$CSTAET>>config_ssh$CEND`
     config_ssh
 
-    echo -e "$CSTAET>>config_network$CEND"
+    echo -e `$CSTAET>>config_network$CEND`
     config_network
 }
 
--- cloudera/06_ntp.sh
+++ cloudera/06_ntp.sh
@@ -9,59 +9,59 @@ set -e
 
 # 移除旧版本 ntp
 function remove_old_ntp() {
-    echo -e "$CSTART>>>>yum remove -y chrony ntp$CEND"
+    echo -e `$CSTART>>>>yum remove -y chrony ntp$CEND`
     yum remove -y chrony ntp
 }
 
 # 安装 ntp
 function install_ntp() {
-    echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr `yum install -y ntp`; done$CEND"
-    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr `yum install -y ntp`; done
+    echo -e `$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr "yum install -y ntp"; done$CEND`
+    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr "yum install -y ntp"; done
 }
 
 # 备份 ntp config
 function backup_ntp_config() {
-    echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr `cp /etc/ntp.conf /etc/ntp.conf.bak`; done$CEND"
-    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr `cp /etc/ntp.conf /etc/ntp.conf.bak`; done
+    echo -e `$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr "cp /etc/ntp.conf /etc/ntp.conf.bak"; done$CEND`
+    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr "cp /etc/ntp.conf /etc/ntp.conf.bak"; done
 }
 
 # 配置 ntp server
 function config_ntp_server() {
-    echo -e "$CSTART>>>>cp config/ntp_server /etc/ntp.conf$CEND"
+    echo -e `$CSTART>>>>cp config/ntp_server /etc/ntp.conf$CEND`
     cp config/ntp_server /etc/ntp.conf
 }
 
 # 配置 ntp clients
 function config_ntp_clients() {
-    echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do scp config/ntp_clients \$ipaddr:/etc/ntp.conf; done$CEND"
+    echo -e `$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do scp config/ntp_clients \$ipaddr:/etc/ntp.conf; done$CEND`
     cat config/vm_info | while read ipaddr name passwd; do scp config/ntp_clients $ipaddr:/etc/ntp.conf; done
 }
 
 # 重启 ntp 服务
 function restart_ntp() {
-    echo -e "$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr `systemctl restart ntpd; systemctl enable ntpd; timedatectl set-ntp true`; done$CEND"
-    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr `systemctl restart ntpd; systemctl enable ntpd; timedatectl set-ntp true`; done
+    echo -e `$CSTART>>>>cat config/vm_info | while read ipaddr name passwd; do ssh \$ipaddr "systemctl restart ntpd; systemctl enable ntpd; timedatectl set-ntp true"; done$CEND`
+    cat config/vm_info | while read ipaddr name passwd; do ssh $ipaddr "systemctl restart ntpd; systemctl enable ntpd; timedatectl set-ntp true"; done
 }
 
 function main() {
-    echo -e "$CSTART>06_ntp.sh$CEND"
+    echo -e `$CSTART>06_ntp.sh$CEND`
 
-    echo -e "$CSTART>>remove_old_ntp$CEND"
+    echo -e `$CSTART>>remove_old_ntp$CEND`
     remove_old_ntp
 
-    echo -e "$CSTART>>install_ntp$CEND"
+    echo -e `$CSTART>>install_ntp$CEND`
     install_ntp
 
-    echo -e "$CSTART>>backup_ntp_config$CEND"
+    echo -e `$CSTART>>backup_ntp_config$CEND`
     backup_ntp_config
 
-    echo -e "$CSTART>>config_ntp_server$CEND"
+    echo -e `$CSTART>>config_ntp_server$CEND`
     config_ntp_server
 
-    echo -e "$CSTART>>config_ntp_clients$CEND"
+    echo -e `$CSTART>>config_ntp_clients$CEND`
     config_ntp_clients
 
-    echo -e "$CSTART>>restart_ntp$CEND"
+    echo -e `$CSTART>>restart_ntp$CEND`
     restart_ntp
 }
 
