3c19b7a4ba3e8bbe0233ec3c378bde7f6b618371
杜森 <sen.du@petgleam.com>
2023-05-06 13:32:48 +0800

    [IMP] # 避免误操作，添加输入密码步骤

--- reboot.sh
+++ reboot.sh
@@ -8,6 +8,17 @@
 set -e 
 source 00_env
 
+# 避免误操作，添加输入密码步骤
+function identification() {
+    read -s -p "请输入密码: " pswd
+    shapswd=$(echo $pswd | sha1sum | head -c 10)
+    if [[ "$shapswd" == "d5b3776603" ]]; then
+        echo && true
+    else
+        echo && false
+    fi
+}
+
 # 重启机器
 function reboot() {
     cat config/vm_info | while read ipaddr name passwd
@@ -19,6 +30,10 @@ function reboot() {
 
 function main() {
     echo -e "$CSTART>reboot.sh$CEND"
+
+    echo -e "$CSTART>>identification$CEND"
+    identification
+
     echo -e "$CSTART>>reboot$CEND"
     reboot
 }
