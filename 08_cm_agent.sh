241e5189e8753a14ebf7bd01a1ccafc4fec0ce8e
杜森 <sen.du@petgleam.com>
2023-06-01 11:40:40 +0800

    [FIX] # 移除旧版本 cloudera manager agent

--- 08_cm_agent.sh
+++ 08_cm_agent.sh
@@ -12,8 +12,17 @@ source 00_env
 function install_dependent() {
     cat config/vm_info | grep -v "^#" | grep -v "^$" | while read ipaddr name passwd
     do 
-        echo -e "$CSTART>>>>$ipaddr$CEND";
-        ssh -n $ipaddr "yum install -y perl perl-devel bind-utils psmisc cyrus-sasl-plain cyrus-sasl-gssapi fuse portmap fuse-libs /lib/lsb/init-functions httpd mod_ssl openssl-devel python-psycopg2 MySQL-python libxslt || true";
+        echo -e "$CSTART>>>>$ipaddr$CEND"
+        ssh -n $ipaddr "yum install -y perl perl-devel bind-utils psmisc cyrus-sasl-plain cyrus-sasl-gssapi fuse portmap fuse-libs /lib/lsb/init-functions httpd mod_ssl openssl-devel python-psycopg2 MySQL-python libxslt || true"
+    done
+}
+
+# 移除旧版本 cloudera manager agent
+function remove_old_agent() {
+    cat config/vm_info | grep -v "^#" | grep -v "^$" | while read ipaddr name passwd
+    do 
+        echo -e "$CSTART>>>>$ipaddr$CEND"
+        ssh -n $ipaddr "yum remove -y cloudera-manager* || true"
     done
 }
 
@@ -21,9 +30,9 @@ function install_dependent() {
 function install_agent() {
     cat config/vm_info | grep -v "^#" | grep -v "^$" | while read ipaddr name passwd
     do 
-        echo -e "$CSTART>>>>$ipaddr$CEND";
-        ssh -n $ipaddr "rpm -ivh $HTTPD_SERVER/cm6/6.3.1/cloudera-manager-daemons-6.3.1-1466458.el7.x86_64.rpm || true";
-        ssh -n $ipaddr "rpm -ivh $HTTPD_SERVER/cm6/6.3.1/cloudera-manager-agent-6.3.1-1466458.el7.x86_64.rpm || true";
+        echo -e "$CSTART>>>>$ipaddr$CEND"
+        ssh -n $ipaddr "rpm -ivh $HTTPD_SERVER/cm6/6.3.1/cloudera-manager-daemons-6.3.1-1466458.el7.x86_64.rpm || true"
+        ssh -n $ipaddr "rpm -ivh $HTTPD_SERVER/cm6/6.3.1/cloudera-manager-agent-6.3.1-1466458.el7.x86_64.rpm || true"
     done
 }
 
@@ -31,7 +40,7 @@ function install_agent() {
 function config_agent() {
     cat config/vm_info | grep -v "^#" | grep -v "^$" | while read ipaddr name passwd
     do 
-        echo -e "$CSTART>>>>$ipaddr$CEND";
+        echo -e "$CSTART>>>>$ipaddr$CEND"
         ssh -n $ipaddr "cp /etc/cloudera-scm-agent/config.ini /etc/cloudera-scm-agent/config.ini.bak" || true
         scp config/cm_agent $ipaddr:/etc/cloudera-scm-agent/config.ini
         ssh -n $ipaddr "sed -i 's/TODO_SERVER_IP/$LocalIp/g' /etc/cloudera-scm-agent/config.ini"
@@ -44,7 +53,7 @@ function config_agent() {
 function restart_agent() {
     cat config/vm_info | grep -v "^#" | grep -v "^$" | while read ipaddr name passwd
     do 
-        echo -e "$CSTART>>>>$ipaddr$CEND";
+        echo -e "$CSTART>>>>$ipaddr$CEND"
         ssh -n $ipaddr "systemctl restart cloudera-scm-agent; systemctl enable cloudera-scm-agent; chkconfig cloudera-scm-agent on"
     done
 }
@@ -54,6 +63,9 @@ function main() {
 
     echo -e "$CSTART>>>>install_dependent$CEND"
     install_dependent
+
+    echo -e "$CSTART>>>>remove_old_agent$CEND"
+    remove_old_agent
 
     echo -e "$CSTART>>>>install_agent$CEND"
     install_agent
